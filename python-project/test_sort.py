86e51c505b2a8501b6efa3a2b857657f34df69e4
ALazyTeacher <151020270+ALazyTeacher@users.noreply.github.com>
2023-11-21 14:46:55 +0000

    Update test_sort.py

--- python-project/test_sort.py
+++ python-project/test_sort.py
@@ -1,7 +1,4 @@
-from sort import sort
-
-def send_msg(channel, msg):
-    print("TECHIO> message --channel \"{}\" \"{}\"".format(channel, msg))
+from sort import mySort
 
 
 def success():
@@ -15,8 +12,8 @@ def fail():
 def test_sort_all():
     try:
         data1 = [5,6,2,3,7,11,1,15]
-        count1 = sort.sort(data1)
-        data1 = sort(data1)
+        count1 = mySort(data1)
+        data1 = data1.sort()
         assert count1 == data1, "It worked!"
         success()
 
